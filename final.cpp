68fd3f28ed5a960f82775c60080cfc20bf5d80b8
Gustavoprogramas <gustavo.vimacedo@gmail.com>
2026-03-02 15:31:39 -0300

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -13,7 +13,8 @@ struct MemoryFlag {
     int value;
 };
 
-HWND hBtn1, hBtn2, hStatus;
+// Adicionado hBtnDll
+HWND hBtnDll, hBtn1, hBtn2, hStatus;
 
 void AplicarPrioridadePersistente() {
     HKEY hKey;
@@ -47,31 +48,25 @@ uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
 }
 
 bool InjetarDLL(HANDLE hProc, const char* dllNome) {
-    // caminho completo da DLL
     char caminhoCompleto[MAX_PATH];
     if (GetFullPathNameA(dllNome, MAX_PATH, caminhoCompleto, NULL) == 0) {
         return false;
     }
 
-   
     LPVOID pDllPath = VirtualAllocEx(hProc, 0, strlen(caminhoCompleto) + 1, MEM_COMMIT, PAGE_READWRITE);
     if (!pDllPath) return false;
 
-   
     WriteProcessMemory(hProc, pDllPath, (LPVOID)caminhoCompleto, strlen(caminhoCompleto) + 1, 0);
 
-    // endereço da função LoadLibraryA do Windows
     HMODULE hKernel32 = GetModuleHandleA("Kernel32.dll");
     LPVOID pLoadLibrary = (LPVOID)GetProcAddress(hKernel32, "LoadLibraryA");
 
-   
     HANDLE hThread = CreateRemoteThread(hProc, NULL, 0, (LPTHREAD_START_ROUTINE)pLoadLibrary, pDllPath, 0, NULL);
     if (!hThread) {
         VirtualFreeEx(hProc, pDllPath, 0, MEM_RELEASE);
         return false;
     }
 
-    
     WaitForSingleObject(hThread, INFINITE);
     CloseHandle(hThread);
     VirtualFreeEx(hProc, pDllPath, 0, MEM_RELEASE);
@@ -79,53 +74,35 @@ bool InjetarDLL(HANDLE hProc, const char* dllNome) {
     return true;
 }
 
+// Nova função apenas para o botão de injeção
+void ExecutarInjecao() {
+    SetWindowTextA(hStatus, "Status: Aguardando Roblox para Injetar DLL...");
+    
+    DWORD procId = 0;
+    while (!procId) {
+        HWND hwnd = FindWindowA(NULL, "Roblox");
+        if (hwnd) GetWindowThreadProcessId(hwnd, &procId);
+        Sleep(1000);
+    }
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+    if (hProc) {
+        if (InjetarDLL(hProc, "memalloc.dll")) {
+            SetWindowTextA(hStatus, "Status: memalloc.dll Injetada com Sucesso!");
+        } else {
+            SetWindowTextA(hStatus, "Status: Falha ao injetar memalloc.dll.");
+        }
+        CloseHandle(hProc);
+    } else {
+        SetWindowTextA(hStatus, "Status: ERRO! Execute como Administrador.");
+    }
+}
+
 void ExecutarOtimizador(int choice) {
     SetWindowTextA(hStatus, "Status: Aguardando RobloxPlayerBeta.exe...");
 
-    /** std::vector<MemoryFlag> flags = {
-        {"TaskSchedulerTargetFps", 0x77958B4, 9999},
-        {"RenderShadowmapBias", 0x6A3B178, -1},
-        {"FastGPULightCulling3", 0x77D7030, 1},
-        {"DebugForceMSAASamples", 0x775B52C, 1},
-        {"FRMMaxGrassDistance", 0x6A46C88, 0},
-        {"DebugFRMQualityLevelOverride", 0x775EAC4, 4},
-        {"HandleAltEnterFullscreenManually", 0x78AB208, 1},
-        {"RakNetLoopMs", 0x6A40538, 1},
-        {"RakNetPingFrequencyMillisecond", 0x6A3F12C, 10},
-        {"ClientPacketMaxDelayMs", 0x6A3ACBC, 1},
-        {"ClientPacketMaxFrameMicroseconds", 0x6A3ACC8, 1},
-        {"ClientPacketHealthyMsPerSecondLimit", 0x6A3ACD4, 1},
-        {"MaxProcessPacketsJobScaling", 0x6A3ACAC, 2139999999},
-        {"MaxProcessPacketsStepsAccumulated", 0x6A3ACB8, 0},
-        {"MaxProcessPacketsStepsPerCyclic", 0x6A3ACB4, 2139999999},
-        {"S2PhysicsSenderRate", 0x6A5286C, 1},
-        {"WorldStepMax", 0x6A3EAC8, 1},
-        {"MaxMissedWorldStepsRemembered", 0x6A3EABC, 1},
-        {"RakNetResendBufferArrayLength", 0x6A40864, 128},
-        {"RakNetResendMaxThresholdTimeInUs", 0x6A40894, 10000},
-        {"RakNetResendMinThresholdTimeInUs", 0x6A40890, 5000},
-        {"RakNetResendRttMultiple", 0x6A4088C, 1},
-        {"RakNetResendScaleBackFactorHundredthsPercent", 0x775E560, 50},
-        {"InterpolationMaxDelayMSec", 0x6A3EB34, 1},
-        {"InterpolationAwareTargetTimeLerpHundredth", 0x6A3EB2C, 100},
-        {"InterpolationDtLimitForLod", 0x6A3EBBC, 1},
-        {"DebugSimPrimalStiffnessMax", 0x6A47370, 0},
-        {"BloomFrmCutoff", 0x6A3AFB0, -1},
-        {"RobloxGuiBlurIntensity", 0x6A560B4, 0},
-        {"RenderLocalLightUpdatesMax", 0x6A3B188, 1},
-        {"ClientPacketExcessMicroseconds", 0x6A3ACC0, 1},
-        {"DiffractionSolverUsesGraphicsQualityLevel", 0x7876538, 3},
-        {"GameNetPVHeaderRotationOrientIdToleranceExponent", 0x6A3ACF4, -1},
-        {"RenderLocalLightUpdatesMin", 0x6A3B184, 1},
-        {"RenderLocalLightFadeInMs", 0x6A3B18C, 0},
-        {"RenderReportGfxQualityLevel", 0x77D2900, 1},
-        {"SimBlockLargeLocalToolWeldManipulationsThreshold", 0x6A3AC84, -1}
-    }; */
-
-    //lista flags antigas:
-          std::vector<MemoryFlag> flags = {
-
-        {"TaskSchedulerTargetFps", 0x76A8730, 9999},
+    std::vector<MemoryFlag> flags = {
+{"TaskSchedulerTargetFps", 0x76A8730, 9999},
 
         {"RenderShadowmapBias", 0x695A144, -1},
 
@@ -212,9 +189,7 @@ void ExecutarOtimizador(int choice) {
         //aq
 
         {"SimBlockLargeLocalToolWeldManipulationsThreshold", 0x6959C8C, -1}
-
     };
-     
 
     DWORD procId = 0;
     while (!procId) {
@@ -229,8 +204,7 @@ void ExecutarOtimizador(int choice) {
     if (hProc && baseAddr) {
         SetWindowTextA(hStatus, "Status: Otimizando Processo...");
 
-        InjetarDLL(hProc, "memalloc.dll");
-        
+        // tirei o bglh coloca dnv se der merda
 
         SetPriorityClass(hProc, HIGH_PRIORITY_CLASS);
 
@@ -268,10 +242,7 @@ void ExecutarOtimizador(int choice) {
             }
         }
 
-
-        
-
-if (choice == 1) {
+        if (choice == 1) {
             SetWindowTextA(hStatus, "Status: Dump injetado. Monitorando...");
             while (true) {
                 DWORD exitCode;
@@ -282,13 +253,8 @@ if (choice == 1) {
                     uintptr_t target = baseAddr + flag.offset;
                     DWORD oldProtect;
 
-                    
                     if (VirtualProtectEx(hProc, (LPVOID)target, sizeof(int), PAGE_EXECUTE_READWRITE, &oldProtect)) {
-                        
-                       
                         WriteProcessMemory(hProc, (LPVOID)target, &flag.value, sizeof(int), NULL);
-                        
-                        
                         VirtualProtectEx(hProc, (LPVOID)target, sizeof(int), oldProtect, &oldProtect);
                     }
                 }
@@ -296,20 +262,14 @@ if (choice == 1) {
             }
             SetWindowTextA(hStatus, "Status: Jogo Fechado.");
         } else {
-            SetWindowTextA(hStatus, "Status: Otimizado . Monitorando...");
-            
+            SetWindowTextA(hStatus, "Status: Otimizado. Monitorando...");
             
             while (true) {
                 DWORD exitCode;
                 GetExitCodeProcess(hProc, &exitCode);
-                
-                
                 if (exitCode != STILL_ACTIVE) break;
-                
                 Sleep(5000); 
             }
-            
-            
             SetWindowTextA(hStatus, "Status: Jogo Fechado.");
         }
 
@@ -322,21 +282,32 @@ if (choice == 1) {
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CREATE:
+            // .
+            hBtnDll = CreateWindowA("BUTTON", "Injetar memalloc.dll",
+                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                20, 20, 250, 40, hwnd, (HMENU)3, NULL, NULL);
+
+            // 2
             hBtn1 = CreateWindowA("BUTTON", "1 - Otimizar e Colocar Bandeiras",
                 WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                20, 20, 250, 40, hwnd, (HMENU)1, NULL, NULL);
+                20, 70, 250, 40, hwnd, (HMENU)1, NULL, NULL);
             
             hBtn2 = CreateWindowA("BUTTON", "2 - Apenas Otimizar",
                 WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                20, 70, 250, 40, hwnd, (HMENU)2, NULL, NULL);
+                20, 120, 250, 40, hwnd, (HMENU)2, NULL, NULL);
 
             hStatus = CreateWindowA("STATIC", "Status: Aguardando escolha...",
                 WS_VISIBLE | WS_CHILD,
-                20, 130, 250, 40, hwnd, NULL, NULL, NULL);
+                20, 175, 250, 40, hwnd, NULL, NULL, NULL);
             break;
 
         case WM_COMMAND:
-            if (LOWORD(wParam) == 1 || LOWORD(wParam) == 2) {
+            if (LOWORD(wParam) == 3) {
+                // 3
+                std::thread(ExecutarInjecao).detach();
+            } 
+            else if (LOWORD(wParam) == 1 || LOWORD(wParam) == 2) {
+                // v
                 EnableWindow(hBtn1, FALSE);
                 EnableWindow(hBtn2, FALSE);
                 std::thread(ExecutarOtimizador, LOWORD(wParam)).detach();
@@ -361,9 +332,10 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     RegisterClassA(&wc);
 
+    // so altura
     HWND hwnd = CreateWindowA("RobloxOptClass", "Roblox Optimizer Engine",
         WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, 
-        CW_USEDEFAULT, CW_USEDEFAULT, 310, 220,
+        CW_USEDEFAULT, CW_USEDEFAULT, 310, 270,
         NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
@@ -376,4 +348,4 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     return 0;
 }
 
-// bash: g++ -o app.exe final.cpp -static -lkernel32 -luser32 -lwinmm -mwindows
+//cmp: g++ -o app.exe final.cpp -static -lkernel32 -luser32 -lwinmm -mwindows
